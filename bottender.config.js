8e3e47f295416c8f815acb5cef2b9b625d113acb
kjohn01 <r01631002@ntu.edu.tw>
2019-11-01 18:18:06 +0800

    FIX CONFIG

--- bottender.config.js
+++ bottender.config.js
@@ -8,16 +8,16 @@ module.exports = {
         maxSize: 500,
       },
     },
-    channels: {
-        messenger: {
-          enabled: true,
-          path: '/',
-          pageId: process.env.MESSENGER_PAGE_ID,
-          accessToken: process.env.MESSENGER_ACCESS_TOKEN,
-          verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
-          appId: process.env.MESSENGER_APP_ID,
-          appSecret: process.env.MESSENGER_APP_SECRET,
-        },
+  },
+  channels: {
+    messenger: {
+      enabled: true,
+      path: '/',
+      pageId: process.env.MESSENGER_PAGE_ID,
+      accessToken: process.env.MESSENGER_ACCESS_TOKEN,
+      verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
+      appId: process.env.MESSENGER_APP_ID,
+      appSecret: process.env.MESSENGER_APP_SECRET,
     },
   },
 };
